d7f8fbfbf4a35d8be51012fb172216295ead5d90
BisquitBehemoth <alexander_zaitsev89@mail.ru>
2016-04-18 16:23:17 +0300

    Registration Activity ready

--- app/src/main/java/alex/beaconapp/AppConfig.java
+++ app/src/main/java/alex/beaconapp/AppConfig.java
@@ -11,10 +11,20 @@ public class AppConfig {
     public static String SHARED_PREFERENCES_EMAIL = "email";
     public static String SHARED_PREFERENCES_PASSWORD = "password";
 
+    //parameters for the user info
+    public static String USER_ID ="userId";
+    public static String USER_TOKEN ="token";
+    public static String USER_NAME ="firstName";
+    public static String USER_LAST_NAME ="lastName";
+    public static String USER_EMAIL ="username";
+    public static String USER_PASSWORD ="password";
+    public static String USER_TYPE ="type";
+
     // Server user login url
     public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+    public static String URL_REGISTER = "http://5.101.107.114:8080/api/register";
+
 
 }
